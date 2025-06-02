95771d77057c2b0e9e5aaca25fabff8569925551
MxDDE02 <maximilian.diehl.de@gmail.com>
2025-06-02 08:24:02 +0200

    Euler with Interpolation
    Euler was used to incorporate interpolation. The predefined step size is used to calculate the steps and interpolation is then used to output the right time intervals.

--- copasi/trajectory/CEuler.cpp
+++ copasi/trajectory/CEuler.cpp
@@ -11,12 +11,15 @@
 #include "copasi/model/CModel.h"
 #include "copasi/utilities/CCopasiMethod.h"
 #include "copasi/model/CModel.h"
+#include <vector>
+#include <iomanip>  // für std::setprecision
 
-//Konstruktoren von directmethod übernommen und rausgenommen, was nicht gebraucht wird - eigentlich nur mTargettime
+
+
 CEulerMethod::CEulerMethod(const CDataContainer * pParent,
                            const CTaskEnum::Method & methodType,
                            const CTaskEnum::Task & taskType): 
-CTrajectoryMethod(pParent, methodType, taskType), //Basisklassenkosntruktot - war bei stochdirect so
+CTrajectoryMethod(pParent, methodType, taskType),
 mTargetTime(0.0), 
 mData(), 
 mpY(NULL), 
@@ -32,9 +35,11 @@ CEulerMethod::CEulerMethod(const CEulerMethod & src,
                            const CDataContainer * pParent): 
 CTrajectoryMethod(src, pParent),
 mTargetTime(0.0), 
+mIntervalSize(0.0),
 mData(), 
 mpY(NULL), 
-mpYdot(NULL)
+mpYdot(NULL), 
+mpYd(NULL)
 {
   assert((void *) &mData == (void *) &mData.dim);
   mData.pMethod = this;
@@ -43,41 +48,65 @@ mpYdot(NULL)
 
 CEulerMethod::~CEulerMethod()
 {
-  //einfach leer 
+  pdeletev(mpYd);
+  pdeletev(mpY);
 }
 
 void CEulerMethod::initializeParameter()
 {
   assertParameter("Step size", CCopasiParameter::Type::DOUBLE, (C_FLOAT64) 0.01);
+  assertParameter("Interpolation", CCopasiParameter::Type::BOOL, true);;
 }
 
-void CEulerMethod::start() 
+void CEulerMethod::start()
 {
+  // 1. Call base class method to initialize container state and time
   CTrajectoryMethod::start();
-  const CTrajectoryProblem * pTP = static_cast<const CTrajectoryProblem *>(mpProblem); //static cast gibt zugriff auf andere Basisklasse 
+
+  // 2. Retrieve the trajectory problem for duration and step size
+  const CTrajectoryProblem * pTP = static_cast<const CTrajectoryProblem *>(mpProblem);
+
+  // 3. Determine the dimension of the system (excluding fixed event targets)
   mData.dim = (C_INT)(mContainerState.size() - mpContainer->getCountFixedEventTargets());
-  pdeletev(mpY);
-  mpY = new C_FLOAT64[mData.dim];
-  pdeletev(mpYd);
-  mpYd = new C_FLOAT64[mData.dim];
-  mTargetTime = *mpContainerStateTime + pTP->getDuration(); //mTargettime wird die Dauer des Problems zugewiesen 
-  mStepsize = getValue< double >("Step size"); //getValue oder getParameeter?
+
+  // 4. Get pointer to rate vector (excluding fixed event targets)
   mpYdot = mpContainer->getRate(*mpReducedModel).array() + mpContainer->getCountFixedEventTargets();
+
+  // 5. Set simulation end time and initial step interval
+  mTargetTime = *mpContainerStateTime + pTP->getDuration();
+  mIntervalSize = *mpContainerStateTime + pTP->getStepSize();
+
+  // 6. Retrieve the integration step size from parameters
+  mStepsize = getValue< double >("Step size");
+
+  // 7. Allocate memory for state (mpY) and derivative (mpYd) vectors
+  mpY = new C_FLOAT64[mData.dim];
+  mpYd = new C_FLOAT64[mData.dim];
+
+  // 8. Copy current state into local state vector
   memcpy(mpY, mpContainerStateTime, mData.dim * sizeof(C_FLOAT64));
+
+  // 9. Ensure step size does not exceed total interval size 
+  /*
+  if (mStepsize > mIntervalSize)
+  {
+    mStepsize = mIntervalSize;
+  }
+  */
 }
 
-
-/**
-   * from Radau5 - also used after start
-   */
-// from Radau5 but just evalF would also be fine I think ????
+/* Uncomment this - if the integrator is written in Fortan 
 void CEulerMethod::EvalF(const C_INT * n, const C_FLOAT64 * t, const C_FLOAT64 * y, C_FLOAT64 * ydot, C_FLOAT64 *, C_INT *)
 {
   static_cast<Data *>((void *) n)->pMethod->evalF(t, y, ydot);
 }
+*/
 
+//Uncoment this - if the evaluation of the math container should be in a seperate function (unnecessary in Euler)
+/*
 void CEulerMethod::evalF(const C_FLOAT64 * t, const C_FLOAT64 * y, C_FLOAT64 * ydot)
 {
+  //The comments just make a backup version of the current mpContainerStateTime 
   //CVector< C_FLOAT64 > yTemp(mData.dim);
   //memcpy(yTemp.array(), mpContainerStateTime, mData.dim * sizeof(C_FLOAT64));
 
@@ -96,43 +125,102 @@ void CEulerMethod::evalF(const C_FLOAT64 * t, const C_FLOAT64 * y, C_FLOAT64 * y
 
   return;
 }
+*/
 
-
-
-CTrajectoryMethod::Status CEulerMethod::step(const double & deltaT,
-                                             const bool & /* final */) //nicht verhandelbar, weil so in der basisklasse
+CTrajectoryMethod::Status CEulerMethod::step(const double & deltaT, const bool & /* final */)
 {
-  C_FLOAT64 EndTime = *mpContainerStateTime + deltaT;
-  //Calculation of step amount 
+  const C_FLOAT64 outputTime = *mpContainerStateTime + deltaT;
 
-  //PROBLEM: wenn letzter Schritt größer ist 
-  while (*mpContainerStateTime<EndTime)
+  //Saving of the initial Math-Container in the mHistory - for interpolation
+  TimeStatePair initial;
+  initial.time = *mpContainerStateTime;
+  initial.state.assign(mpY, mpY + mData.dim);
+  initial.rate.assign(mpYdot, mpYdot + mData.dim);
+  mHistory.push_back(initial);
+
+
+  while (*mpContainerStateTime < outputTime)
   {
-    doSingleStep(*mpContainerStateTime, *mpContainerStateTime + mStepsize);
+    doSingleStep(*mpContainerStateTime);
   }
-  return NORMAL; //muss glaub weil bei step in TrajectoryMethods normal FAILURE zurückgegeben wird standartmäßig
+  
+  //Interpolation -> get the Trajectory Problem defined state at the requested time 
+  std::vector<C_FLOAT64> interpolatedState = interpolateAt(outputTime);
+  memcpy(mpContainerStateTime, interpolatedState.data(), mData.dim * sizeof(C_FLOAT64));
+
+
+  *mpContainerStateTime = outputTime;
+
+  return NORMAL;
 }
 
-
-// Euler Schritt um durch math container durchzuitterieren y(t+1, y+1) = y(t,y) + h * f`(t,y) 
-// ich muss auf die Startwerte zugreifen 
-// und auf die rechte Seite der ODEs 
-// do one step würde dann die startwerte in die rechte seite einsetzen, um die Steigung zu bekommen bedeutet yn + h * f(yn)
-C_FLOAT64 CEulerMethod::doSingleStep(C_FLOAT64 startTime, const C_FLOAT64 & endTime)
+C_FLOAT64 CEulerMethod::doSingleStep(C_FLOAT64 startTime)
 {
-  C_FLOAT64 h = mStepsize;
-  evalF(&startTime, mpY, mpYd); 
-
-  for (int i = 0; i < mData.dim; ++i)
-{
-   mpY[i] = mpY[i] + h * mpYd[i];
-}
-  // Zeit vorschieben
-  *mpContainerStateTime += h;
-
+  // Calclate the rates => evalF 
+  if (mpY != mpContainerStateTime)
   memcpy(mpContainerStateTime, mpY, mData.dim * sizeof(C_FLOAT64));
   mpContainer->updateSimulatedValues(false);
+  memcpy(mpYd, mpYdot, mData.dim * sizeof(C_FLOAT64)); 
 
-return *mpContainerStateTime;
+  // Euler-Step: y = y + h * f(y)
+    for (int i = 0; i < mData.dim; ++i)
+    {
+      mpY[i] = mpY[i] + mStepsize* mpYd[i];
+    }
 
+    // Update the time 
+    *mpContainerStateTime += mStepsize;
+
+    // writes the result in the math container 
+    memcpy(mpContainerStateTime, mpY, mData.dim * sizeof(C_FLOAT64));
+    //mpContainer->updateSimulatedValues(false);
+
+    // Save each Euler-Step result in the mHistory 
+    TimeStatePair ts;
+    ts.time = *mpContainerStateTime;
+    ts.state.assign(mpY, mpY + mData.dim);
+    ts.rate.assign(mpYd, mpYd + mData.dim);
+    mHistory.push_back(ts);
+  }
+
+
+
+std::vector<C_FLOAT64> CEulerMethod::interpolateAt(C_FLOAT64 t) const
+{
+  //does nothing if no integration has been done 
+  if (mHistory.empty()) return {}; 
+  // precaution: if the time is smaller then the starttime (=0) the starting state will be returned 
+  if (t <= mHistory.front().time) return mHistory.front().state; 
+  // prectaution: if the time is bigger then the endtime , the end state will be returned 
+  if (t >= mHistory.back().time) return mHistory.back().state;
+ 
+  // Find the interpolation - intervall borders 
+  for (size_t i = 1; i < mHistory.size(); ++i)
+  {
+    // precaution: if a entry for the requested time already exists
+    if (mHistory[i].time == t) return mHistory[i].state;
+    //search for the first time point, which is bigger then t => right border 
+    else if (mHistory[i].time > t)
+    {
+      // time points which should interpolate 
+      const TimeStatePair& p0 = mHistory[i - 1]; //left border 
+      const TimeStatePair& p1 = mHistory[i]; //right border 
+      //if Euler interpolation is requested 
+      //C_FLOAT64 h0 = t - p0.time; 
+
+      //vector for saving the interpolated state at time t 
+      std::vector<C_FLOAT64> interpolated(mData.dim);
+      for (int j = 0; j < mData.dim; ++j)
+      {
+        //linear interpolation: euler step with different step size 
+        //interpolated[j] = p0.state[j] + h0 * p0.rate[j];
+
+        //linear interpolation y = y1 + ((x-x1)/(x2-x1)) * (y2-y1)
+        interpolated[j] = p0.state[j] + ((t - p0.time) / (p1.time - p0.time)) * (p1.state[j] - p0.state[j]);
+
+      } 
+
+      return interpolated;
+    }
+  }
 }
--- copasi/trajectory/CEuler.h
+++ copasi/trajectory/CEuler.h
@@ -10,53 +10,124 @@ public:
     size_t dim;
     CEulerMethod * pMethod;
   };
+  /**
+   *  Struct to safe each intermediate step of the euler method
+   */
+  struct TimeStatePair
+  {
+    C_FLOAT64 time;
+    std::vector<C_FLOAT64> state;
+    std::vector<C_FLOAT64> rate;
+  };
+  /**
+   *  Construtor 
+   */
   CEulerMethod(const CDataContainer * pParent,
                const CTaskEnum::Method & methodType,
                const CTaskEnum::Task & taskType);
-
+/**
+   *  Copy-Construtor 
+   */
   CEulerMethod(const CEulerMethod & src,
             const CDataContainer * pParent);
 
-  virtual ~CEulerMethod();
+/**
+   *  Deconstructor 
+   * => deletes the used pointers (mpYd and mpY)
+   */
+  ~CEulerMethod(); 
 
+/**
+   * 1. calls the start function of TrajectoryMethod to initialize the mContainerstate and time
+   * 2. initializing of different parameters: 
+   * - mData.dim (dimension of the ode problem)
+   * - mpYdot (array of rates)
+   * - mTargetTime to get the value of the duration (from TrajectoryProblem)
+   * - mStepsize to get the value of the step sizes given in the initializeParamter-function
+   * - mpY and mpYd as an empy pointer array of size mData.dim  
+   * - then copies the state vector into mpY 
+   */
   virtual void start();
+/**
+   *  Does the Euler integration until the first time interval is reached (Trajectory Problem)
+   *  - does euler steps (with the predefinded step size)
+   *  - interpolates, if the step size is bigger then the intervall size 
+   */
+  virtual CTrajectoryMethod::Status step(const double & deltaT, const bool & final); //added virtual to that 
 
-  CTrajectoryMethod::Status step(const double & deltaT, const bool & final);
+  /**
+   *  The Euler steps 
+   */
+  C_FLOAT64 doSingleStep(C_FLOAT64 startTime);
 
-  C_FLOAT64 doSingleStep(C_FLOAT64 startTime, const C_FLOAT64 & endTime);
+  /**
+    * Returns a linearly interpolated state for the given time.
+    *
+    * @param t  The time at which the interpolated state is requested.
+    * @return   A vector containing the interpolated state.
+    */
+   std::vector<C_FLOAT64> interpolateAt(C_FLOAT64 t) const;
+
 
  /**
-   *  This evaluates the derivatives
+   *  This evaluates the derivatives 
+   * => uses the state vector to calculate a new rate vector 
    */
-  static void EvalF(const C_INT * n, const C_FLOAT64 * t, const C_FLOAT64 * y, C_FLOAT64 * ydot, C_FLOAT64 *, C_INT *);
-
-  virtual void evalF(const C_FLOAT64 * t, const C_FLOAT64 * y, C_FLOAT64 * ydot);
+  //virtual void evalF(const C_FLOAT64 * t, const C_FLOAT64 * y, C_FLOAT64 * ydot);
 
 
 
 protected:
+  /**
+   * Function to initialize parameters. 
+   * Just determines the step size used for the Euler Method. 
+   * The default is 0.01.
+   */
   void initializeParameter();
 
 private:
+  /**
+   * Float, which stores the duration of the TrajectoryProblem
+   */
   C_FLOAT64 mTargetTime;
+
+/**
+   * Float, which stores the step size from the initialized parameter "Step size"
+   */
   C_FLOAT64 mStepsize;
-    /**
-   * mData.dim is the dimension of the ODE system.
-   * mData.pMethod contains CLsodaMethod * this to be used
-   * in the static method EvalF
+  /**
+   * mData.dim is the dimension of the ODE system. 
+   * It is used to determine the size of the state vector
    */
   Data mData;
 
     /**
-   * Pointer to the array with left hand side values of the math container.
+   * Pointer to the array with left hand side values of the math container (=state vector).
+   * => time and concentration of the species 
    */
   C_FLOAT64 * mpY;
 
   /**
-   * Pointer to the array with right hand side values of the math container.
+   * Pointer to the array with right hand side values of the math container (=rate vector).
+   * => only used for the evalF function 
    */
   const C_FLOAT64 * mpYdot;
 
+  /**
+   * Pointer to the array with the rates of the reactions evaluated (=rate vector - intermediate)
+   * => used to determine the rate of each equation with a given mpY - more used as temporary storage
+   * 
+   */
   C_FLOAT64 * mpYd;
-  // KEINE zusätzlichen Vektoren hier!
+
+/**
+   * Float, which stores the intervall size of the TrajectoryProblem
+   */
+  C_FLOAT64 mIntervalSize; 
+
+  /**
+    * A history of time and state pairs stored during the integration.
+    * This is used for linear interpolation to obtain the state at arbitrary times.
+    */
+  std::vector<TimeStatePair> mHistory;
 };
