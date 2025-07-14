894e6ce778061aa01862aceec51cba67a69902ab
MxDDE02 <maximilian.diehl.de@gmail.com>
2025-07-14 19:00:48 +0200

    added some safety for root ausführung

--- copasi/trajectory/CRadau5Method.cpp
+++ copasi/trajectory/CRadau5Method.cpp
@@ -284,7 +284,7 @@ CTrajectoryMethod::Status CRadau5Method::step(const double & deltaT,
                   for (; pRootFound != pRootFoundEnd; ++pRootFound, ++pRootValue)
                   // Added a numerical Tolerance just to make sure 
                     //if (*pRootValue == RootValue || *pRootValue == -RootValue)
-                    if (std::fabs(*pRootValue - RootValue) < Tolerance || std::fabs(*pRootValue + RootValue) < Tolerance)
+                    if (std::fabs(*pRootValue - RootValue) < Tolerance || std::fabs(*pRootValue + RootValue) < Tolerance ||std::fabs(*pRootValue + 0) < Tolerance)
                       {
                         *pRootFound = static_cast< C_INT >(CMath::RootToggleType::ToggleBoth);
                       }
